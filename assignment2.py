567fafbc1443420730eda2e0907e02e1eb0d88cb
TusharKulkarni04253N <kulkarnit498@gmail.com>
2025-04-22 13:51:52 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,32 @@
+# assignment 2
+
+# problem 1
+
+a = int(input("Insert digit between 0 to 10:- "))
+
+if (a == 1):
+    print('given number is odd')
+
+elif(a == 3):
+    print('given number is odd')
+
+elif(a == 5):
+    print('given number is odd')
+
+elif(a == 7):
+    print('given number is odd')
+
+elif(a == 9):
+    print('given number is odd')
+
+else :
+    print('given number is even')
+
+# problem 2
+
+a = 0
+
+for i in range(1,51):
+    a=a+i
+
+print('The sum of numbers from 1 to 50 is: ' + str(a))
